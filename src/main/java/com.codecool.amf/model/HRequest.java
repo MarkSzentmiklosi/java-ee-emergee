20d8e8ce599dd738c69a1903480ad97a7823d9ef
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-29 14:30:33 +0200

    Configured fields for JPA

--- src/main/java/com.codecool.amf/model/HRequest.java
+++ src/main/java/com.codecool.amf/model/HRequest.java
@@ -2,14 +2,23 @@ package com.codecool.amf.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-
+@Entity
 public class HRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int userId;
+    @ManyToOne
+    private User user;
+    @Column(name = "creation_date")
     private Timestamp requestCreationDate;
     private boolean active = true;
-    private int partnerId;
-    private String location;
+    @Column(name = "partner_id")
+    @ManyToOne
+    private Partner partner;
+    @OneToOne
+    private Coordinate coordinate;
+    @Transient
+    private Address address;
 
 }
 
