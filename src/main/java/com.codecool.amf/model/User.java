5160432734348a641afce88e7b08095df2911e49
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-29 11:05:11 +0200

    Package names contained wrong path

--- src/main/java/com.codecool.amf/model/User.java
+++ src/main/java/com.codecool.amf/model/User.java
@@ -1,16 +1,14 @@
-package main.java.com.codecool.amf.model;
+package com.codecool.amf.model;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
 public class User {
-    @Id
     private int id;
     private String phoneNumber;
     private String name;
     private String email;
-    private Address address;
+    private HomeAddress address;
     private String idCardNum;
     private List<HRequest> requests;
 
