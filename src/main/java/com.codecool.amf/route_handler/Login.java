34408fe767e85260113fa37a7c62183f68d16dee
akosKoroknay <koroknayakos@gmail.com>
2018-05-30 10:53:21 +0200

    Created login html form with thymeleaf data access from java

--- src/main/java/com.codecool.amf/route_handler/Login.java
+++ src/main/java/com.codecool.amf/route_handler/Login.java
@@ -2,6 +2,7 @@ package com.codecool.amf.route_handler;
 
 import com.codecool.amf.auth.AuthenticationManager;
 import com.codecool.amf.config.TemplateEngineUtil;
+import com.codecool.amf.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/login"})
@@ -26,9 +28,15 @@ public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String email = req.getParameter("email");
+        String username = req.getParameter("username");
         String password = req.getParameter("password");
-
         String hashedPassword = AuthenticationManager.hashPassword(password);
+
+        // TODO: Get hashed password from db with the corresponding email address and compare it with the ones given by user
+        // TODO: Handling exception (redirect to error page)
+            HttpSession session = req.getSession();
+            session.setAttribute("userId", 1);
+            //TODO: Get the id of the logged in user and store it in session
+            resp.sendRedirect("/");
     }
 }
