8d621131429666c6e66fb57f326c30f3d93c7907
akosKoroknay <koroknayakos@gmail.com>
2018-05-30 13:49:45 +0200

    New select query template created: selectUserByEmail

--- src/main/java/com.codecool.amf/jpa/QueryManager.java
+++ src/main/java/com.codecool.amf/jpa/QueryManager.java
@@ -7,4 +7,11 @@ import java.util.List;
 public class QueryManager {
     private final EntityManager entityManager = JpaManager.getInstance();
 
+    public List selectUserByEmail(String email) {
+        String queryString = "SELECT U FROM User U WHERE U.email = :email";
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter("email", email);
+        return query.getResultList();
+    }
+
 }
