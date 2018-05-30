044018d0edd9a33482a1ede70a0a646c7c578f78
akosKoroknay <koroknayakos@gmail.com>
2018-05-30 11:00:45 +0200

    passwordHash field added for User

--- src/main/java/com.codecool.amf/model/User.java
+++ src/main/java/com.codecool.amf/model/User.java
@@ -21,4 +21,6 @@ public class User {
     private String idCardNum;
     @OneToMany(mappedBy = "user")
     private List<HRequest> requests = new ArrayList<>();
+    @Column(name="hash")
+    private String passwordHash;
 }
