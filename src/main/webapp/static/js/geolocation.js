9a644b1c923d5d4c4331c0a496bd4f3e4d3924ca
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-30 21:27:45 +0200

    Gets the user's current coordinates, then forwarding json to the server which is generated by HERE api

--- src/main/webapp/static/js/geolocation.js
+++ src/main/webapp/static/js/geolocation.js
@@ -1,5 +1,11 @@
 var $btn = $("#geo_location_btn");
-var $locDiv = $("#location");
+var street;
+var city;
+var zip;
+var country;
+var houseNum;
+var label;
+
 
 $btn.click(getLocation);
 
@@ -9,11 +15,33 @@ function getLocation() {
     }
 }
 
+
 function sendPosition(position) {
-    $.post("/coordinates",{
-        latitude:position.coords.latitude,
-        longitude:position.coords.longitude
-    },function () {
+    const url = `https://reverse.geocoder.cit.api.here.com/6.2/reversegeocode.json?prox=${position.coords.latitude}%2C${position.coords.longitude}%2C250&mode=retrieveAddresses&maxresults=1&gen=8&app_id=Ta4Bs3skhxzoMXfIP4j8&app_code=y0khbGyyfAoVakL5lqjipg`
+
+    $.getJSON(url, function (data) {
+        label = data['Response']['View'][0]['Result'][0]['Location']['Address']['Label'];
+        country = data['Response']['View'][0]['Result'][0]['Location']['Address']['Country'];
+        city = data['Response']['View'][0]['Result'][0]['Location']['Address']['City'];
+        zip = data['Response']['View'][0]['Result'][0]['Location']['Address']['postalCode'];
+        street = data['Response']['View'][0]['Result'][0]['Location']['Address']['Street'];
+        houseNum = data['Response']['View'][0]['Result'][0]['Location']['Address']['HouseNumber'];
+
+        $("#loc").text("Are you here? \n" + label);
+        $("#myModal").modal("show");
+
+        $("#yesBtn").click(function () {
+            $.post("/service", {
+                label: label,
+                country: country,
+                city: city,
+                zip: zip,
+                houseNum: houseNum,
+                street: street
+            })
+        });
+
 
     })
+
 }
