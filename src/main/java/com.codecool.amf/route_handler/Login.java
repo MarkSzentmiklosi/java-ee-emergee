5e40d5907bba2bd0f91e852fd31ff6ae111fdcbb
akosKoroknay <koroknayakos@gmail.com>
2018-05-30 10:07:57 +0200

    Login route created with logn html template

---
+++ src/main/java/com.codecool.amf/route_handler/Login.java
@@ -0,0 +1,36 @@
+package com.codecool.amf.route_handler;
+
+import com.codecool.amf.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("login.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        String hashedPassword = PasswordManager.hashPassword(password);
+
+        RegistrationDaoJdbc registrationDaoJdbc = RegistrationDaoJdbc.getInstance();
+        registrationDaoJdbc.add(email, hashedPassword);
+    }
+}
