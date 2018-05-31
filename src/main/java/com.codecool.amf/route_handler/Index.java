6f0244cc25b4eaccc4f1b585a02aa78e46eaaec8
akosKoroknay <koroknayakos@gmail.com>
2018-05-31 13:51:30 +0200

    Index page redirects to Login page if there isn't a logged in user in session

--- src/main/java/com.codecool.amf/route_handler/Index.java
+++ src/main/java/com.codecool.amf/route_handler/Index.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/"})
@@ -16,6 +17,10 @@ public class Index extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") == null) {
+            resp.sendRedirect("/login");
+        }
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         engine.process("index.html", context, resp.getWriter());
