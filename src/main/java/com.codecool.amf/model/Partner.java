2e381116f2c3df3d2bb87cecc491457cfceeb246
Ferenc Bolla <bollaferenc@gmail.com>
2018-05-31 02:53:01 +0200

    Created Location class which contains the position of the emergency call and created constructors and setters getters for every model class

--- src/main/java/com.codecool.amf/model/Partner.java
+++ src/main/java/com.codecool.amf/model/Partner.java
@@ -5,13 +5,65 @@ import java.util.List;
 
 @Entity
 public class Partner {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
+
     private String email;
+
     @Enumerated
     private PService service;
+
     @OneToMany(mappedBy = "partner")
     private List<HRequest> requests;
+
+
+    public Partner() {
+    }
+
+    public Partner(String name, String email, PService service, List<HRequest> requests) {
+        this.name = name;
+        this.email = email;
+        this.service = service;
+        this.requests = requests;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public PService getService() {
+        return service;
+    }
+
+    public void setService(PService service) {
+        this.service = service;
+    }
+
+    public List<HRequest> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<HRequest> requests) {
+        this.requests = requests;
+    }
 }
