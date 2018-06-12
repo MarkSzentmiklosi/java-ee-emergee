bd85dcd59456ddf46016519c74f1afa1c4e3301e
Ferenc Bolla <bollaferenc@gmail.com>
2018-06-12 10:40:51 +0200

    Change servlet environment so custom servlet constructors can be used

---
+++ src/main/java/com.codecool.amf/initServlet/InitializationServlet.java
@@ -0,0 +1,9 @@
+package com.codecool.amf.initServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class InitializationServlet extends HttpServlet {
+    public void init() throws ServletException {
+    }
+}
