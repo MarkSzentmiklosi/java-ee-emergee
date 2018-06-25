7ba0d7a8385a71fb0c2ea8d37b6ccdf93ec2f74e
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-06-25 16:24:43 +0200

    CheckUserLogin uses Spring boot

--- src/main/java/com.codecool.amf/route_handler/CheckRegistrationEmail.java
+++ src/main/java/com.codecool.amf/route_handler/CheckRegistrationEmail.java
@@ -2,6 +2,7 @@ package com.codecool.amf.route_handler;
 
 import com.codecool.amf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -11,7 +12,6 @@ public class CheckRegistrationEmail{
     @Autowired
     UserService userService;
 
-    @ResponseBody
     @RequestMapping(value = "/check_registration_email",method = RequestMethod.POST)
     public String checkEmail(@RequestParam(name = "email") String email) {
         String response = "newEmail";
--- src/main/java/com.codecool.amf/route_handler/CheckUserLogin.java
+++ src/main/java/com.codecool.amf/route_handler/CheckUserLogin.java
@@ -1,51 +1,47 @@
-//package com.codecool.amf.route_handler;
-//
-//import com.codecool.amf.authenticator.AuthenticationManager;
-//import com.codecool.amf.jpa.QueryManager;
-//import com.codecool.amf.model.User;
-//
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//import java.util.List;
-//
-//public class CheckUserLogin extends HttpServlet {
-//    private QueryManager queryManager;
-//    private AuthenticationManager authenticationManager;
-//
-//    public CheckUserLogin(QueryManager queryManager, AuthenticationManager authenticationManager) {
-//        this.queryManager = queryManager;
-//        this.authenticationManager = authenticationManager;
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//
-//        String inputEmail = req.getParameter("email");
-//        String inputPassword = req.getParameter("password");
-//
-//        String response = "invalid";
-//
-//        List users = queryManager.selectUserByEmail(inputEmail);
-//
-//        if (users.size() != 0) {
-//            User user = (User) users.get(0);
-//            String userPassword = user.getPasswordHash();
-//
-//            if (isPasswordMatch(inputPassword, userPassword)) {
-//                HttpSession session = req.getSession();
-//                session.setAttribute("user", user);
-//                response = "valid";
-//            }
-//        }
-//
-//        resp.getWriter().write(response);
-//
-//    }
-//
-//    private boolean isPasswordMatch(String inputPassword, String userPassword) {
-//        return authenticationManager.checkPassword(inputPassword, userPassword);
-//    }
-//}
+package com.codecool.amf.route_handler;
+
+import com.codecool.amf.authenticator.AuthenticationManager;
+import com.codecool.amf.model.User;
+import com.codecool.amf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class CheckUserLogin {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/check_user_login", method = RequestMethod.POST)
+    public String checkUserLogin(@RequestParam(name = "email") String email,
+                                 @RequestParam(name = "password") String password,
+                                 HttpSession session) {
+
+        String response = "invalid";
+
+        User user = userService.getUserByEmail(email);
+        if (user != null) {
+            String savedPassword = user.getPasswordHash();
+            if (isPasswordMatch(password, savedPassword)) {
+                session.setAttribute("user", user);
+                response = "valid";
+            }
+
+        }
+
+        return response;
+    }
+
+
+    private boolean isPasswordMatch(String inputPassword, String userPassword) {
+        return authenticationManager.checkPassword(inputPassword, userPassword);
+    }
+}
