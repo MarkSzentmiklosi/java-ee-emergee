b61b0c7899257deb5aa81ce71148f3d9753f1b4e
Ferenc Bolla <bollaferenc@gmail.com>
2018-06-25 15:44:22 +0200

    Registration route uses Spring environment

--- src/main/java/com.codecool.amf/route_handler/Registration.java
+++ src/main/java/com.codecool.amf/route_handler/Registration.java
@@ -1,29 +1,23 @@
-//package com.codecool.amf.route_handler;
-//
-//import com.codecool.amf.config.TemplateEngineUtil;
-//import org.thymeleaf.TemplateEngine;
-//import org.thymeleaf.context.WebContext;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//
-//public class Registration extends HttpServlet {
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        HttpSession session = req.getSession();
-//
-//        String email = req.getParameter("registration_email");
-//        String password = req.getParameter("registration_password");
-//
-//        session.setAttribute("password", password);
-//        session.setAttribute("email", email);
-//        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-//        WebContext context = new WebContext(req, resp, req.getServletContext());
-//        engine.process("update-profile.html", context, resp.getWriter());
-//    }
-//}
+package com.codecool.amf.route_handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class Registration {
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registartion(HttpSession session,
+                               @RequestParam(name = "registration_email") String email,
+                               @RequestParam(name = "registration_password") String password) {
+
+        session.setAttribute("password", password);
+        session.setAttribute("email", email);
+        return "update-profile";
+    }
+
+}
