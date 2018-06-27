e58496ae051c535399e9b3bcd1771900e9ae819c
Ferenc Bolla <bollaferenc@gmail.com>
2018-06-27 14:56:17 +0200

    Test IndexService

--- src/test/com/codecool/amf/service/IndexServiceTest.java
+++ src/test/com/codecool/amf/service/IndexServiceTest.java
@@ -1,7 +1,42 @@
 package com.codecool.amf.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codecool.amf.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-class IndexServiceTest {
+import javax.servlet.http.HttpSession;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndexServiceTest {
+
+    @Autowired
+    IndexService indexService;
+
+    @Mock
+    private
+    HttpSession session;
+
+    @Mock
+    private
+    User user;
+
+    @Test
+    public void testIfUserNotLoggedInRedirectToLoginPage() {
+        when(session.getAttribute("user")).thenReturn(null);
+        assertEquals("login", indexService.handleIndexGet(session));
+    }
+
+    @Test
+    public void testIfUserLoggedInRedirectToIndexPage() {
+        when(session.getAttribute("user")).thenReturn(user);
+        assertEquals("index", indexService.handleIndexGet(session));
+    }
 }
