cb1252fc393fac93946ea3479e1d9f8c5eaddd41
akosKoroknay <koroknayakos@gmail.com>
2018-06-27 11:22:09 +0200

    Business logic moved from SaveProfileSettings controller controller to UserService

--- src/main/java/com.codecool.amf/service/UserService.java
+++ src/main/java/com.codecool.amf/service/UserService.java
@@ -1,5 +1,6 @@
 package com.codecool.amf.service;
 
+import com.codecool.amf.model.Address;
 import com.codecool.amf.model.User;
 import com.codecool.amf.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AddressService addressService;
 
     public void saveUser(User user) {
         userRepository.save(user);
@@ -32,5 +36,37 @@ public class UserService {
     public void updatePhoneNumber(User user, String phoneNumber) {
         user.setPhoneNumber(phoneNumber);
         userRepository.save(user);
+    }
+
+    public void updateUserProfile(User currentUser, String dataType, String input) {
+        User user = getUserByEmail(currentUser.getEmail());
+        Address address = user.getAddress();
+
+        switch (dataType) {
+            case "name":
+                updateUserName(user, input);
+                break;
+            case "country":
+                addressService.updateCountry(address, input);
+                break;
+            case "city":
+                addressService.updateCity(address, input);
+                break;
+            case "zipCode":
+                addressService.updateZip(address, input);
+                break;
+            case "street":
+                addressService.updateStreet(address, input);
+                break;
+            case "houseNumber":
+                addressService.updateHouseNumber(address, input);
+                break;
+            case "phoneNumber":
+                updatePhoneNumber(user, input);
+                break;
+            case "idCardNum":
+                updateIdCardNum(user, input);
+                break;
+        }
     }
 }
