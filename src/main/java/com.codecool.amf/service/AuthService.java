422d70ba87c551f1faf60b73b5ccfb939b3798f6
akosKoroknay <koroknayakos@gmail.com>
2018-06-27 10:40:05 +0200

    Business logic moved from CheckUserLogin controller to AuthService

--- src/main/java/com.codecool.amf/service/AuthService.java
+++ src/main/java/com.codecool.amf/service/AuthService.java
@@ -1,13 +1,13 @@
 package com.codecool.amf.service;
 
+import com.codecool.amf.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
-@Component
+@Service
 public class AuthService {
 
     @Autowired
@@ -22,12 +22,32 @@ public class AuthService {
         }
     }
 
-    public String handleCheckRegistrationEmailPost(@RequestParam(name = "email") String email) {
+    public String handleCheckRegistrationEmailPost(String email) {
         String response = "newEmail";
 
         if (userService.getUserByEmail(email) != null) {
             response = "existingEmail";
         }
         return response;
+    }
+
+    public String handleCheckUserLoginPost(String email, String password, HttpSession session) {
+        String response = "invalid";
+
+        User user = userService.getUserByEmail(email);
+        if (user != null) {
+            String savedPassword = user.getPasswordHash();
+            if (isPasswordMatch(password, savedPassword)) {
+                session.setAttribute("user", user);
+                response = "valid";
+            }
+
+        }
+
+        return response;
+    }
+
+    private boolean isPasswordMatch(String inputPassword, String userPassword) {
+        return passwordService.checkPassword(inputPassword, userPassword);
     }
 }
