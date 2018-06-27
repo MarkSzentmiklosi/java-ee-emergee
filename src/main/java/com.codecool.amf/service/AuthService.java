539e820a18f333096510d42f695ce9f260229606
akosKoroknay <koroknayakos@gmail.com>
2018-06-27 10:21:53 +0200

    Business logic moved from ChackRegistrationEmail controller to AuthService

--- src/main/java/com.codecool.amf/service/AuthService.java
+++ src/main/java/com.codecool.amf/service/AuthService.java
@@ -1,12 +1,17 @@
 package com.codecool.amf.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
 @Component
 public class AuthService {
+
+    @Autowired
+    UserService userService;
 
     public String handleLoginGet(HttpSession session, Model model) {
         if (session.getAttribute("user") != null) {
@@ -15,5 +20,14 @@ public class AuthService {
             model.addAttribute("error", false);
             return "login";
         }
+    }
+
+    public String handleCheckRegistrationEmailPost(@RequestParam(name = "email") String email) {
+        String response = "newEmail";
+
+        if (userService.getUserByEmail(email) != null) {
+            response = "existingEmail";
+        }
+        return response;
     }
 }
