76bd67527f7a63d12a3f3a4e6308104b03c2fbef
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-06-28 17:30:55 +0200

    Get all requests from DB for logged in partner

--- src/main/java/com.codecool.amf/service/PartnerService.java
+++ src/main/java/com.codecool.amf/service/PartnerService.java
@@ -1,18 +1,25 @@
 package com.codecool.amf.service;
 
+import com.codecool.amf.model.HelpRequest;
 import com.codecool.amf.model.Partner;
 import com.codecool.amf.model.ServiceType;
+import com.codecool.amf.repository.HelpRequestRepository;
 import com.codecool.amf.repository.PartnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Service
 public class PartnerService {
 
     @Autowired
     PartnerRepository partnerRepository;
+
+    @Autowired
+    HelpRequestRepository helpRequestRepository;
 
     public Partner selectPartnerByServiceType(ServiceType serviceType) {
         return partnerRepository.findByService(serviceType);
@@ -26,10 +33,14 @@ public class PartnerService {
         partnerRepository.save(partner);
     }
 
-    public String handleIndexGet(HttpSession session) {
+    public String handleIndexGet(HttpSession session, Model model) {
         if (session.getAttribute("partnerId") == null) {
             return "redirect:/partner-login";
         }
+        long partnerId = (Long) session.getAttribute("partnerId");
+        List<HelpRequest> requests = helpRequestRepository.findByPartnerId(partnerId);
+
+        model.addAttribute("requests", requests);
         return "partner_index";
     }
 }
